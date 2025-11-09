abaf52b365c7b9a1ede99ff09d78ba801b29dec1
sabamiso <nei271824@gmail.com>
2025-11-09 21:02:40 +0900

    add comments on teleoperation.py

--- Python-damiao-test/teleoperation.py
+++ Python-damiao-test/teleoperation.py
@@ -38,17 +38,10 @@ serial_device = serial.Serial('COM6', 921600, timeout=0.5)          #follower ar
 serial_device2 = serial.Serial('COM7', 921600, timeout=0.5)         #follower arm STS
 serial_device3 = serial.Serial('COM5', 921600, timeout=0.5)         #leader arm DM
 serial_device4 = serial.Serial('COM9', 921600, timeout=0.5)         #leader arm STS
-MotorControl1=MotorControl(serial_device)
-MotorControl2=MotorControl(serial_device2)  #STS motors
-MotorControl3=MotorControl(serial_device3)
-MotorControl4=MotorControl(serial_device4)  #for reading STS motors
-
-
-for i in range(5):
-    MotorControl1.addMotor(motorlist[i])
-
-for i in range(5):
-    MotorControl3.addMotor(motorlist2[i])
+MotorControl1=MotorControl(serial_device)   #follower DM motors
+MotorControl2=MotorControl(serial_device2)  #follower STS motors
+MotorControl3=MotorControl(serial_device3)  #leader DM motors
+MotorControl4=MotorControl(serial_device4)  #leader STS motors
 
 controllerid = 0x0f
 
@@ -56,12 +49,19 @@ motorid = 1
 motorid2 = 2
 
 for i in range(5):
-    if MotorControl1.switchControlMode(motorlist[i],Control_Type.MIT):
-        print("1st arm motor"+str(i)+" switch MIT success")
+    MotorControl1.addMotor(motorlist[i])
+
+for i in range(5):
+    MotorControl3.addMotor(motorlist2[i])
+
+
+for i in range(5):
+    if MotorControl1.switchControlMode(motorlist[i+1],Control_Type.MIT):
+        print("1st arm motor"+str(i+1)+" switch MIT success")
     
 for i in range(5):
     if MotorControl3.switchControlMode(motorlist2[i],Control_Type.MIT):
-        print("2nd arm motor"+str(i)+" switch MIT success")
+        print("2nd arm motor"+str(i+1)+" switch MIT success")
 
 
 
@@ -91,25 +91,32 @@ start = time.time()
 #while time.time() - start < 10:
 while keyboard.is_pressed('s')==False:
     l=[]
-    MotorControl4.STSControl_read(controllerid, motorid)
-    for i in range(5):
+    MotorControl4.STSControl_read(controllerid, motorid)        #leader sts id=1 read(同時に二つやると処理が追い付かなくなる)
+    for i in range(5):          #leader DM motors read
         MotorControl3.controlMIT(motorlist[i], 0, 0, 0, 0,  0)
 
     time.sleep(0.005)
     
+    MotorControl3.recv()   #leader DM motors recv (値の取得命令送信後、値が返ってくるまでタイムラグあり)
     for i in range(5):
-        l.append(motorlist2[i].getPosition())
-    MotorControl4.STSControl_read(controllerid, motorid2)
-    l.append(MotorControl4.sts_map[motorid])
-    MotorControl2.STSControl_write(controllerid, motorid, l[5])
+        l.append(motorlist2[i].getPosition())   #leader DM motors position append
+    MotorControl4.STSControl_read(controllerid, motorid2)   #leader sts id=2 read （同時に二つやると処理が追い付かなくなる）
+    l.append(MotorControl4.sts_map[motorid])        #leader sts id=1 position append (id=1(１つ目のモーターの値が返ってくるのがこのぐらいと予想))
+    MotorControl2.STSControl_write(controllerid, motorid, l[5])   #follower sts id=1 write(値戻ってきたのでfollowerに書く)
     
-    l.append( MotorControl4.sts_map[motorid2])
     for i in range(5):
         MITMaxTorque(MotorControl1, motorlist[i], l[i], K_p, 0, 1)
-    MotorControl2.STSControl_write(controllerid, motorid2, l[6])
+    MotorControl4.recv()   #leader STS recv(id=2) (値の取得命令送信後、値が返ってくるまでタイムラグあり)
+    l.append( MotorControl4.sts_map[motorid2])      #leader sts id=2 position append
+    MotorControl2.STSControl_write(controllerid, motorid2, l[6])    #follower sts id=2 write(値戻ってきたのでfollowerに送信)
     
     print([float(q) for q in l])
 
-    #data.append(l)
     idx+=1
     time.sleep(0.005)
+
+
+    #data.append(l)
+    #   この辺にcsv保存処理を書きたい
+    #
+    #
